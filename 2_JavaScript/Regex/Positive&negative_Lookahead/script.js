be83e720e374f9a67c29485f0d7135224f6b2568
RadekKap <radkap@st.amu.edu.pl>
2020-03-05 12:35:10 +0100

    Remove unnecessary folders

--- 2_JavaScript/Regex/Positive&negative_Lookahead/script.js
+++
@@ -1,4 +0,0 @@
-let sampleWord = "bana12";
-let pwRegex = /^(?!\d)(?=\w{4,}(?=\d{2}))(?=\D*)/; // Change this line
-let result = pwRegex.test(sampleWord);
-console.log(result);
--- 2_JavaScript/Regex/Restrict_Possible_Usernames/script.js
+++
@@ -1,4 +0,0 @@
-let username = "c573";
-let userCheck = /^[a-z]([a-z]+|[0-9]{2})\d*$/i; // Change this line
-let result = userCheck.test(username);
-console.log(result);
